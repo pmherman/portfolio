fe1e5ca6e871dad5a9f810d08c7a9184baf8c19a
Paul M Herman <paul.herman@accesso.com>
2020-04-14 11:35:55 -0400

    no message

---
+++ php/contact.php
@@ -0,0 +1,51 @@
+<?php
+
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
+
+        // Check that data was sent to the mailer.
+        if (empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Please complete the form and try again.";
+            exit;
+        }
+
+        // Set the recipient email address.
+        // FIXME: Update this to your desired email address.
+        $recipient = "pmherman85@icloud.com";
+
+        // Set the email subject.
+        $subject = "Request for Consultation: $email";
+
+        // Build the email content.
+        $email_content = "Full Name: $name\n";
+        $email_content .= "Email: $email\n";
+        $email_content .= "Message:\n$message\n";
+        // Build the email headers.
+        $email_headers = "From: $name <$email>";
+
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Your message has been sent successfully!";
+            echo "<script>window.location.replace('../../index.html')</script>";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+
+?>
+
